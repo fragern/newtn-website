c8c55a14c4d0ed9552712ee30384baa47e1099c0
elianrichard <elianrichards@gmail.com>
2025-02-03 16:42:45 +0700

    added prettier and husky

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
-
 export default function About() {
   return (
     <>
-      <p className="font-generalsans font-light">
-        About
-      </p>
+      <p className="font-generalsans font-light">About</p>
     </>
   );
 }
