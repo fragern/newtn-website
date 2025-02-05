6c7a7b88925dddda32e25e90c47e72dc440a9404
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-02-05 10:45:47 +0700

    style(our story section): fix logo, padding, and add image to background

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,30 +1,36 @@
 export default function About() {
   return (
-    <div className="flex flex-col">
-      <section className="flex flex-col gap-20 px-20 py-32 lg:flex-row lg:items-center lg:justify-center lg:gap-40 lg:px-32">
-        <div className="flex flex-col gap-5 text-4xl">
-          <p className="font-semibold">We are</p>
-          <p className="text-8xl font-black">Newtn.</p>
+    <div className="relative flex flex-col items-center">
+      <img
+        src="/assets/images/snowflake.svg"
+        className="absolute left-0 top-16 -z-10"
+        alt="Snowflake"
+      />
+
+      <section className="flex flex-col gap-20 px-10 py-64 lg:flex-row lg:items-center lg:justify-between lg:gap-40">
+        <div className="flex w-fit flex-none flex-col gap-5 text-4xl">
+          <p className="text-4xl font-semibold text-primary">We are</p>
+          <img
+            src="/assets/logos/newtn/text/navy.svg"
+            className="w-80"
+            alt="Text Logo"
+          />
         </div>
 
-        <div className="flex flex-col gap-6">
-          <h1 className="text-4xl font-medium text-primary">Our Story</h1>
-          <p className="text-base text-primary">
-            <span className="font-bold">Newtn</span> was born from a shared
-            passion for innovation and creativity. We are a team of talented
-            developers and designers who crossed paths at the Apple Developer
-            Academy. After graduation, we knew we wanted to stay together and
-            create something meaningful. Inspired by the iconic story of Isaac
-            Newton and the falling apple—a symbol of curiosity and discovery—we
-            embraced the spirit of turning ideas into reality. That&apos;s how
-            Newtn came to life, blending our skills and vision to build
+        <div className="flex flex-1 flex-col gap-6">
+          <p className="text-2xl text-primary">
+            Newtn was founded by a team of passionate developers and designers
+            driven by innovation. Inspired by Newton&apos;s apple—a symbol of
+            curiosity and discovery—we turn ideas into reality, creating
             impactful digital solutions.
           </p>
         </div>
       </section>
 
-      <section className="flex flex-col gap-20 px-20 py-32 lg:flex-row lg:items-center lg:justify-center lg:gap-40 lg:px-32">
-        <h1 className="text-4xl font-medium text-primary">Newtn Talents</h1>
+      <section className="flex flex-col items-center">
+        <h1 className="text-center text-4xl font-semibold text-primary">
+          Newtn Talents
+        </h1>
 
         <div className=""></div>
       </section>
