79aba6a2af41b34b848c03a4c23d87ac425a656f
Christopher Nathanael Tessy <christopher.nathanael1217@gmail.com>
2025-04-29 12:50:11 +0700

    fix: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { motion, useAnimation } from "framer-motion";
 import { NewtnService } from "@/data/newtn-service";
 import { assets2D3DPortfolios } from "@/data/portfolio/2d-3d-assets";
+import { allPortfolios } from "@/data/portfolio/all-portfolios";
 import { appDevelopmentPortfolios } from "@/data/portfolio/app-development";
 import { brandingPortfolios } from "@/data/portfolio/branding";
 import {
@@ -16,9 +17,6 @@ import {
 import { webDevelopmentPortfolios } from "@/data/portfolio/web-development";
 import { type Portfolio } from "@/types/Portfolio";
 import { type PortfolioCategory } from "@/types/PortfolioCategory";
-import CardStack from "./portfolio/containers/CardStack";
-
-const text = "we turn ideas into reality.";
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] =
@@ -53,81 +51,53 @@ export default function Home() {
   }, [inView, controls, hasAnimated]);
 
   return (
-    <>
-      <main className="lg:pt-10">
-        <div className="min-h-full">
-          <div className="flex justify-center px-12 md:justify-between">
-            <div className="z-10 flex flex-col gap-y-20 pt-16">
-              <div className="relative h-[56px] md:h-[138px] md:w-[692px]">
-                <Image
-                  alt="newtn-text"
-                  src={"/assets/logos/newtn/text/text-logo.webp"}
-                  fill
-                  className="object-contain"
-                  sizes="100vw"
-                />
-              </div>
+    <main>
+      <section className="relative flex w-full justify-center px-4 pb-64 pt-32">
+        <div className="absolute -bottom-48 left-16 h-96 w-32 bg-primary"></div>
+        <div className="absolute -bottom-48 left-56 h-96 w-32 bg-primary"></div>
+        <div className="absolute -bottom-48 left-96 h-96 w-32 bg-primary"></div>
 
-              <div className="flex flex-col gap-0 text-center text-2xl font-bold text-[#2F4157] text-stroke-1 text-stroke-white md:gap-2 md:text-left md:text-5xl">
-                <p>Versatility.</p>
-                <p>Innovation.</p>
-                <p>Fresh Perspective.</p>
-              </div>
-            </div>
+        <div className="flex w-full max-w-5xl flex-col items-center gap-16">
+          <div className="relative flex flex-col gap-4 overflow-visible">
+            <div className="absolute inset-0 -z-10 m-auto mt-1 size-96 rounded-full bg-[#C7D9E5] blur-[128px]"></div>
 
-            <div className="absolute right-0 z-0 pt-20 md:relative md:pt-0">
-              <div className="flex flex-col items-end gap-4">
-                <div className="clip-path-triangle h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                <div className="flex gap-4">
-                  <div className="clip-path-triangle h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                  <div className="h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                </div>
-                <div className="flex gap-4">
-                  <div className="clip-path-triangle h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                  <div className="h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                  <div className="clip-path-triangle h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                </div>
-                <div className="flex gap-4">
-                  <div className="clip-path-triangle h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                  <div className="h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                  <div className="clip-path-triangle h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                  <div className="h-[67px] w-[67px] bg-primary transition-all duration-300 hover:border hover:border-primary hover:bg-white md:h-[156px] md:w-[156px]"></div>
-                </div>
-              </div>
-            </div>
+            <h1 className="text-center text-5xl font-semibold text-primary">
+              Your end-to-end tech partner with a product-first mindset
+            </h1>
+
+            <p className="text-center text-2xl font-light text-primary">
+              From idea to launch, Newtn delivers full-cycle product development
+              — combining sharp design, agile engineering, and reliable
+              deployment to turn bold ideas into impactful digital products.
+            </p>
           </div>
 
-          <div ref={ref} className="px-2 pb-14 pt-[150px] md:px-8 md:pt-24">
-            <h1 className="text-center text-base font-normal text-[#2F4157] md:text-5xl">
-              <b>Newtn</b> is a software studio uniting digital talent. <br />
-              Rooted in curiosity and innovation,{" "}
-              <motion.span>
-                {text.split(" ").map((word, i) => (
-                  <span key={i} className="mr-2 underline">
-                    {word.split("").map((letter, j) => (
-                      <motion.span
-                        key={j}
-                        initial={{ opacity: 0 }}
-                        animate={controls}
-                        variants={{ visible: { opacity: 1 } }}
-                        transition={{ delay: i * 0.5 + j * 0.1 }}
-                      >
-                        {letter}
-                      </motion.span>
-                    ))}
-                  </span>
-                ))}
-              </motion.span>
-            </h1>
+          <div className="flex items-center justify-center gap-4">
+            <button
+              type="submit"
+              className="rounded-full border border-primary bg-primary px-6 py-2 text-xl font-medium text-white transition-all hover:bg-white hover:text-primary"
+            >
+              Let&apos;s Talk
+            </button>
+
+            <button
+              type="submit"
+              className="rounded-full border border-primary bg-white px-6 py-2 text-xl font-medium text-primary transition-all hover:bg-primary hover:text-white"
+            >
+              Explore our work
+            </button>
           </div>
         </div>
+      </section>
 
-        <div className="relative flex flex-col gap-10 bg-[#C7D9E5] px-8 py-10 text-[#2F4157] md:mt-[120px] md:gap-20 md:py-20">
-          <div className="flex flex-col justify-center gap-[10px]">
-            <h1 className="text-center text-2xl font-semibold md:text-[30px]">
+      <section className="flex w-full justify-center bg-white/50 px-4 pt-32 backdrop-blur-sm">
+        <div className="flex w-full max-w-5xl flex-col items-center gap-16">
+          <div className="flex w-full max-w-3xl flex-col gap-4">
+            <h1 className="text-center text-4xl font-semibold text-primary">
               End-to-End Services, Delivered Effortlessly
             </h1>
-            <p className="text-center text-sm md:text-lg">
+
+            <p className="text-center text-xl font-light text-primary">
               We specialize in comprehensive digital solutions, delivering
               end-to-end services that encompass asset creation, UI/UX design,
               development, and seamless deployment. Our expertise ensures a
@@ -136,118 +106,162 @@ export default function Home() {
             </p>
           </div>
 
-          <div className="relative flex flex-col gap-8 md:flex-row">
-            {NewtnService.map((service, index) => (
-              <div
-                className="relative z-10 rounded-[20px] bg-white shadow-lg"
-                key={index}
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
+            <div className="flex flex-col gap-6 rounded-3xl bg-white p-8 drop-shadow-xl">
+              <span className="h-24 w-24 bg-orange-300"></span>
+
+              <div className="flex flex-col gap-2">
+                <h2 className="text-3xl font-semibold text-primary">
+                  Development
+                </h2>
+
+                <p className="text-base text-primary">
+                  Crafting responsive, user-friendly websites and building
+                  intuitive, high-performance mobile apps for iOS and Android,
+                  tailored to your business needs.
+                </p>
+              </div>
+
+              <div className="flex flex-wrap gap-3">
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Back-end
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  iOS
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Android
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Cross-Platform
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Front-end
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Website
+                </span>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-6 rounded-3xl bg-white p-8 drop-shadow-xl">
+              <span className="h-24 w-24 bg-orange-300"></span>
+
+              <div className="flex flex-col gap-2">
+                <h2 className="text-3xl font-semibold text-primary">Design </h2>
+
+                <p className="text-base text-primary">
+                  Creating stunning app and web designs, strong brand
+                  identities, and high-quality 2D & 3D assets with engaging
+                  motion graphics, all driven by in-depth UX research.
+                </p>
+              </div>
+
+              <div className="flex flex-wrap gap-3">
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  App Design
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Web Design
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  Branding
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  3D/2D Asset
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  2D Motion
+                </span>
+
+                <span className="rounded-full border border-primary bg-white px-6 py-2 text-base font-semibold text-primary">
+                  UX Research
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="flex w-full justify-center px-4 py-32">
+        <div className="flex w-full max-w-5xl flex-col gap-8">
+          <div className="flex w-full max-w-3xl flex-col gap-4">
+            <h1 className="text-5xl font-bold text-primary">
+              Newtn&apos;s Projects
+            </h1>
+
+            <p className="text-xl text-primary">Web Development</p>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+            {selectedPortfolios.slice(0, 2).map((portfolio) => (
+              <Link
+                href={`/portfolio/${portfolio.category.slug}/${portfolio.id}`}
+                key={portfolio.id}
+                className="flex flex-col overflow-hidden rounded-xl border border-primary bg-white transition-all hover:shadow-xl"
               >
-                <div className="relative h-auto w-full">
+                <div className="relative aspect-[4/3] w-full bg-gray-200">
                   <Image
-                    src={service.image}
-                    width={500}
-                    height={0}
-                    alt={service.image}
-                    className="h-auto w-full object-contain object-top"
+                    src={portfolio.showcaseImage2}
+                    alt={`${portfolio.title}'s image`}
+                    fill
+                    sizes="(max-width: 768px) 100vw, 50vw"
+                    className="object-cover"
+                    priority
                   />
                 </div>
-                <div className="px-6 pt-5">
-                  <h2 className="text-center text-2xl font-semibold text-black md:text-3xl">
-                    {service.title}
-                  </h2>
-                  <p className="px-0 pt-2 text-center text-sm text-[#515151] md:px-6 md:text-base">
-                    {service.description}
+                <div className="p-4">
+                  <h1 className="text-xl font-semibold">{portfolio.title}</h1>
+                  <p className="mt-1 text-gray-600">
+                    {portfolio.short_description}
                   </p>
-                  <div className="mb-8 mt-10 flex flex-wrap justify-center gap-2 px-0 md:gap-5 md:px-9">
-                    {service.tools.map((tools, index) => (
-                      <div
-                        className="rounded-full bg-teal px-3 py-1 text-sm font-semibold text-white md:px-5 md:py-2"
+                  <div className="mt-4 flex flex-wrap gap-2">
+                    {portfolio.tags.map((tag, index) => (
+                      <span
                         key={index}
+                        className="rounded-full border border-primary bg-white px-3 py-1 font-semibold text-primary"
                       >
-                        {tools}
-                      </div>
+                        {tag}
+                      </span>
                     ))}
                   </div>
                 </div>
-              </div>
+              </Link>
             ))}
-
-            <Image
-              alt="bottom-left"
-              src={"/assets/images/home/bottom-left.svg"}
-              width={300}
-              height={300}
-              className="absolute -left-8 bottom-[-40px] z-0 hidden md:block lg:w-[300px]"
-            />
-            <Image
-              alt="bottom-right"
-              src={"/assets/images/home/bottom-right.svg"}
-              width={300}
-              height={300}
-              className="absolute -right-8 bottom-[-40px] z-0 hidden md:block lg:w-[300px]"
-            />
-          </div>
-        </div>
-
-        <div className="relative pt-8">
-          <Image
-            alt="top-left"
-            src={"/assets/images/home/top-left.svg"}
-            width={300}
-            height={300}
-            className="absolute left-0 top-0 -z-10 hidden md:block lg:w-[300px]"
-          />
-
-          <h1 className="relative z-10 flex justify-center text-center text-xl font-semibold text-[#2F4157] md:text-3xl">
-            Selected Projects
-          </h1>
-
-          <div className="relative z-10 flex flex-wrap items-center justify-center gap-4 px-3 pt-6">
-            {PortfolioCategories.map((category, index) => (
-              <button
-                key={index}
-                onClick={() => setSelectedCategory(category)}
-                className={`flex items-center gap-2 text-sm text-[#2F4157] transition-all duration-500 ease-in-out md:text-base ${
-                  selectedCategory.id === category.id
-                    ? "font-semibold text-[#2F4157]"
-                    : "opacity-50"
-                }`}
-              >
-                <div
-                  className={`h-5 w-5 rounded-full bg-primary ${
-                    selectedCategory.id === category.id
-                      ? "bg-primary"
-                      : "opacity-50"
-                  }`}
-                ></div>
-                {category.name}
-              </button>
-            ))}
-          </div>
-
-          <div className="relative z-10 w-full pt-10">
-            <CardStack
-              portfolios={selectedPortfolios}
-              onScrollEnd={handleScrollEnd}
-            />
           </div>
 
           <Link
-            href={"/portfolio"}
-            className="relative z-10 mx-4 mb-20 mt-5 flex w-[205px] justify-center rounded-full bg-primary px-6 py-3.5 text-center text-white opacity-60 lg:mx-auto"
+            href="/portfolio"
+            className="flex items-center gap-2 self-center border-b border-transparent hover:border-primary"
           >
-            Load more project
-          </Link>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              className="h-6 w-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"
+              />
+            </svg>
 
-          <Image
-            alt="bottom-right-portfolio"
-            src={"/assets/images/home/bottom-right-portfolio.svg"}
-            width={300}
-            height={300}
-            className="absolute bottom-0 right-0 -z-10 hidden md:block lg:w-[300px]"
-          />
+            <span className="flex gap-1">
+              <p className="text-xl text-primary">See all work</p>
+              <p className="text-sm">({allPortfolios.length})</p>
+            </span>
+          </Link>
         </div>
-      </main>
-    </>
+      </section>
+    </main>
   );
 }
