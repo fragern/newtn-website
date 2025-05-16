85d80a020ebd300a8a732508a2788caceaaa5710
Nafis <nafisfragern@gmail.com>
2025-05-16 22:27:10 +0700

    [style] talent card mobile view

--- src/app/_components/NewtnTalentCard.tsx
+++ src/app/_components/NewtnTalentCard.tsx
@@ -8,7 +8,7 @@ interface NewtnTalentCardProps {
 const NewtnTalentCard: React.FC<NewtnTalentCardProps> = ({ talent }) => {
   return (
     <Link href={talent.linkedIn} target="_blank" className="w-full">
-      <div className="hover:bg-primary/5 mb-2 flex w-full flex-row items-center justify-between rounded-full border border-transparent px-4 py-2 transition-all duration-300 ease-in-out hover:border-primary hover:shadow-md">
+      <div className="hover:bg-primary/5 mb-2 flex w-full flex-row items-center justify-between rounded-full border border-transparent px-2 py-2 transition-all duration-300 ease-in-out hover:border-primary hover:shadow-md sm:px-4">
         <p className="w-3/5 text-lg font-medium text-primary transition-colors duration-300 sm:text-3xl">
           {talent.name}
         </p>
